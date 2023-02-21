6b45833ae7e4b2792228542b38007b98a91c6e94
kunta-kante <rahmankamara@icloud.com>
2023-02-21 16:25:51 +0100

    nos metier fini

--- src/pages/Métier.js
+++ src/pages/Métier.js
@@ -7,6 +7,7 @@ import {
 } from "react-icons/md";
 import { AiOutlineSetting } from "react-icons/ai";
 import { ImMap } from "react-icons/im";
+import { FcMindMap } from "react-icons/fc";
 import "./Métier.css";
 
 function Métier() {
@@ -19,109 +20,112 @@ function Métier() {
           </h2>
         </div>
         <div className="main-section">
-          <div class="card-right">
-            <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card carriere">
-                <div class="card-body my-card-body">
-                  <div class="text-icon">
-                    <p>
-                      <MdPhoneIphone size={50} color="white" />
-                    </p>
-                    <h5 class="card-title">Editeur de logiciel</h5>
-                  </div>
+          <div class="row">
+            <div class="card-right">
+              <div class="col-sm-6 mb-3 mb-sm-0 my-card">
+                <div class="card carriere">
+                  <div class="card-body my-card-body">
+                    <div class="text-icon">
+                      <p>
+                        <MdPhoneIphone size={50} color="white" />
+                      </p>
+                      <h5 class="card-title">Editeur de logiciel</h5>
+                    </div>
 
-                  <ul class="text-ul">
-                    <li>
-                      Nous développons pour l'ensemble des activités, le
-                      logiciel Comet qui tire profit du Cloud et de sa valeur
-                      ajoutée. La gestion du parc d'instruments est intégrée.
-                    </li>
-                    <li>
-                      Un respect strict des réglementations afin de délivrer un
-                      produit conforme. Un véritable outil d'aide à la décision
-                      pour le technicien lorsqu'il réalise ses inspections et
-                      vérifications.
-                    </li>
-                    <li>
-                      Il s'appuie également sur une gestion électronique de
-                      documents qui facilite la gestion du système qualité ! La
-                      base de données intègre tous les documents réglementaires,
-                      techniques et qualité dont le technicien a besoin pour
-                      mener à bien sa mission
-                    </li>
-                  </ul>
+                    <ul class="text-ul">
+                      <li class="text-li-right">
+                        Nous développons pour l'ensemble des activités, le
+                        logiciel Comet qui tire profit du Cloud et de sa valeur
+                        ajoutée. La gestion du parc d'instruments est intégrée.
+                      </li>
+                      <li class="text-li-right">
+                        Un respect strict des réglementations afin de délivrer
+                        un produit conforme. Un véritable outil d'aide à la
+                        décision pour le technicien lorsqu'il réalise ses
+                        inspections et vérifications.
+                      </li>
+                      <li class="text-li-right">
+                        Il s'appuie également sur une gestion électronique de
+                        documents qui facilite la gestion du système qualité !
+                        La base de données intègre tous les documents
+                        réglementaires, techniques et qualité dont le technicien
+                        a besoin pour mener à bien sa mission
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card carriere">
-                <div class="card-body my-card-body ">
-                  <div class="text-icon">
-                    <p>
-                      <MdHighQuality size={50} color="white" />
-                    </p>
-                    <h5 class="card-title">Système Qualité</h5>
-                  </div>
+              <div class="col-sm-6 mb-3 mb-sm-0 my-card">
+                <div class="card carriere">
+                  <div class="card-body my-card-body ">
+                    <div class="text-icon">
+                      <p>
+                        <MdHighQuality size={50} color="white" />
+                      </p>
+                      <h5 class="card-title">Système Qualité</h5>
+                    </div>
 
-                  <ul class="text-ul">
-                    <li>
-                      Un système qualité multi-activités qui a fait ses preuves,
-                      audité et validé par 4 organismes depuis prés de 17 ans !
-                    </li>
-                    <li>
-                      Que vous pratiquiez une ou plusieurs activités
-                      réglementées dans votre entreprise, un seul système est
-                      utilisé.
-                    </li>
-                    <li>
-                      Les documents dématérialisés sont centralisés dans le
-                      logiciel Comet, permettant ainsi une gestion documentaire
-                      allégée et libère le temps de vos techniciens qui peuvent
-                      se concentrer sur leur métier. Dés qu'un document est
-                      modifié par le siège l'ensemble des adhérents est à jour
-                      instantanément.
-                    </li>
-                  </ul>
+                    <ul class="text-ul">
+                      <li class="text-li-right">
+                        Un système qualité multi-activités qui a fait ses
+                        preuves, audité et validé par 4 organismes depuis prés
+                        de 17 ans !
+                      </li>
+                      <li class="text-li-right">
+                        Que vous pratiquiez une ou plusieurs activités
+                        réglementées dans votre entreprise, un seul système est
+                        utilisé.
+                      </li>
+                      <li class="text-li-right">
+                        Les documents dématérialisés sont centralisés dans le
+                        logiciel Comet, permettant ainsi une gestion
+                        documentaire allégée et libère le temps de vos
+                        techniciens qui peuvent se concentrer sur leur métier.
+                        Dés qu'un document est modifié par le siège l'ensemble
+                        des adhérents est à jour instantanément.
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card carriere">
-                <div class="card-body my-card-body">
-                  <div class="text-icon">
-                    <p>
-                      <MdPhoneIphone size={50} color="white" />
-                    </p>
-                    <h5 class="card-title">La mutualisation</h5>
-                  </div>
+              <div class="col-sm-6 mb-3 mb-sm-0 my-card">
+                <div class="card carriere">
+                  <div class="card-body my-card-body">
+                    <div class="text-icon">
+                      <p>
+                        <FcMindMap size={50} color="white" />
+                      </p>
+                      <h5 class="card-title">La mutualisation</h5>
+                    </div>
 
-                  <ul class="text-ul">
-                    <li>
-                      Tirez profit des retours d'expérience de nos nombreux
-                      adhérents, la consolidation et le partage des
-                      connaissances est notre leitmotiv. En intégrant le réseau
-                      vous n'êtes plus seul dans votre entité face aux
-                      différents acteurs.
-                    </li>
-                    <li>
-                      D'autre part nous rejoindre, c'est aussi profiter d'une
-                      récurrence moins importante des audits externes des
-                      institutionnels et donc de rationaliser vos coûts liés.
-                    </li>
-                    <li>
-                      Enfin, nous concentrons vos besoins de consommables afin
-                      de vous proposer les produits dont vous avez besoin à des
-                      prix intéressants, sur la boutique en ligne. Le catalogue,
-                      c'est vous qui le définissez!
-                    </li>
-                  </ul>
+                    <ul class="text-ul">
+                      <li class="text-li-right">
+                        Tirez profit des retours d'expérience de nos nombreux
+                        adhérents, la consolidation et le partage des
+                        connaissances est notre leitmotiv. En intégrant le
+                        réseau vous n'êtes plus seul dans votre entité face aux
+                        différents acteurs.
+                      </li>
+                      <li class="text-li-right">
+                        D'autre part nous rejoindre, c'est aussi profiter d'une
+                        récurrence moins importante des audits externes des
+                        institutionnels et donc de rationaliser vos coûts liés.
+                      </li>
+                      <li class="text-li-right">
+                        Enfin, nous concentrons vos besoins de consommables afin
+                        de vous proposer les produits dont vous avez besoin à
+                        des prix intéressants, sur la boutique en ligne. Le
+                        catalogue, c'est vous qui le définissez!
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <div class="card-left">
             <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card left-cards">
+              <div class="card my-left-card">
                 <div class="card-body my-card-body">
                   <div class="text-icon">
                     <p>
@@ -151,7 +155,7 @@ function Métier() {
               </div>
             </div>
             <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card left-cards">
+              <div class="card my-left-card">
                 <div class="card-body my-card-body">
                   <div class="text-icon">
                     <p>
@@ -176,7 +180,7 @@ function Métier() {
               </div>
             </div>
             <div class="col-sm-6 mb-3 mb-sm-0 my-card">
-              <div class="card left-cards">
+              <div class="card my-left-card">
                 <div class="card-body my-card-body">
                   <div class="text-icon">
                     <p>
--- src/pages/Réclamations.js
+++ src/pages/Réclamations.js
@@ -1,10 +1,137 @@
 import React from "react";
+import "./Réclamations.css";
 
 function Réclamations() {
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log("Form submitted");
+  // };
+
   return (
-    <section className="hero">
-      <h1>Welcome to nav</h1>
-    </section>
+    <div className="container">
+      <div class="my-form">
+        <div>
+          <h5 class="form-header">
+            La description du processus de traitement des réclamations et des
+            appels peut être mis à disposition sur simple demande.
+          </h5>
+        </div>
+        <form class="row g-3 needs-validation" novalidate>
+          <div class="select-list">
+            <label for="validationCustom01" class="form-label">
+              Activité concernée
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Choisissez une Option</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class=" col-md-4 Société">
+            <label for="validationCustom01" class="form-label">
+              Société
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="validationCustom01"
+              value="Mark"
+              required
+            />
+          </div>
+
+          <div class="civilité">
+            <div class="sex-option">
+              <label for="validationCustom01" class="form-label">
+                Civilité
+              </label>
+              <select class="select-sex" aria-label="Default select example">
+                <option value="1">M.</option>
+                <option value="2">Mille</option>
+                <option value="3">Mme</option>
+              </select>
+            </div>
+
+            <div class="contact-name">
+              <label for="validationCustom02" class="form-label">
+                Nom du contact
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom02"
+                value="Nom du contact"
+                required
+              />
+            </div>
+          </div>
+          <div class="col-md-4 address">
+            <label for="validationCustom02" class="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="validationCustom02"
+              value="Address"
+              required
+            />
+            <div class="valid-feedback">Looks good!</div>
+          </div>
+
+          <div class="personal-info">
+            <div class="contact-info">
+              <label for="validationCustom02" class="form-label">
+                Téléphone
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom02"
+                value=""
+                required
+              />
+            </div>
+
+            <div class="contact-info">
+              <label for="validationCustom02" class="form-label">
+                E-mail *
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="validationCustom02"
+                value=""
+                required
+              />
+            </div>
+          </div>
+
+          <div class="button-sub">
+            <button class="btn btn-primary" type="submit">
+              Submit form
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div class="section-footer">
+        <span class="email-text">
+          <h6 class="section-text">E-mail</h6>
+          <p class="section-text">contact@cercleoptima.com</p>
+        </span>
+        <span class="email-text">
+          <h6 class="section-text">Téléphone</h6>
+          <p class="section-text">04 42 50 96 90</p>
+        </span>
+        <span class="email-text">
+          <p class="section-text">
+            Cercle Optima 31 avenue Francis Perrin 13106 Rousset Cedex France
+          </p>
+        </span>
+      </div>
+    </div>
   );
 }
 
