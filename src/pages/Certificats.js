131f5b653393e41364538c135f4a3fee1ec60c3c
abdulrahman-kamara <rahmankamara@icloud.com>
2023-04-26 13:39:11 +0200

    searchbox implemented with searchfilter

--- src/pages/Certificats.js
+++ src/pages/Certificats.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from "react";
 import supervisionService from "../Context/SupervisionService";
 import Card from "../components/Card/Card"
 import { motion } from "framer-motion";
+import "./Carrières.css"
 
 function Certificats() {
 const [data, setData] = useState([])
-const [searchTerm, setSearchTerm] = useState('');
+const [searchItems, setsearchItems] = useState('');
 
 
 useEffect(() => {
@@ -20,6 +21,7 @@ await supervisionService
   .getAllAdherentAgrements(numero_adherent)
   .then((response) =>{ setData(response)
   console.log("data", response);
+  console.log("data", data[4]);
   })
   
 };
@@ -27,16 +29,18 @@ await supervisionService
 
 const searchFilter = (data) => {
   return data.filter((item) => {
-    if (item && item.titre_agrement && item.titre_agrement.toString().toLowerCase().includes(searchTerm.toLowerCase())) {
+    if (item && item.titre_agrement && item.titre_agrement.toString().toLowerCase().includes(searchItems.toLowerCase())) {
       return item;
     }
   });
 };
   return (
-    <div style={{display:"flex",  justifyContent:"center", alignContent:"center", backgroundColor:"white", marginTop:"2rem",}}>
-  
-    <div>
-      {data && data.map((data, i) =>(
+    <div style={{display:"flex", flexDirection:"column",  justifyContent:"center", alignItems:"center", backgroundColor:"white", marginTop:"2rem", padding:"20px"}}>
+ 
+    <input className="form-control"  type="text" value={searchItems} placeholder="search" onChange={(e) => setsearchItems(e.target.value)} style={{ display:"flex", borderRadius:"5px", padding:"5px", maxWidth:"50rem", flexWrap: "wrap", marginBottom:"1rem", border:"2px solid #ccc"}}/>
+ 
+    <div style={{ display: "flex",  flexDirection:"column", flexWrap: "wrap", }}>
+      { searchFilter(data).map((data, i) =>(
         <Card key={i} data={data}/>
       ) )}
     </div>
@@ -47,4 +51,3 @@ const searchFilter = (data) => {
 export default Certificats;
 
 
-  {/* <input type="text" placeholder="Search by numéro d'adhérent" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} /> */}
